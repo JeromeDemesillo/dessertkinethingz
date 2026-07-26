516efd3fb5882eb2bcba2161587425a4774f4471
CrazedHearts <jeromedemesillo@gmail.com>
2026-07-26 11:12:27 -1000

    add transparent logo and replace with current logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => (
       fluid
       className="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center py-5"
     >
-      <Image src="/logo.png" width={150} height={150} alt="Logo" className="mb-4" />
+      <Image src="/logo-transparent.png" width={150} height={150} alt="Logo" className="mb-4" />
       <div>
         <h1>Jesus & Coffee</h1>
         <p className="mt-3">It's about proclaiming Jesus</p>
