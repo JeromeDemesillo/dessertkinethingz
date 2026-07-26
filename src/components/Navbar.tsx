bfc948389e697aae87d86f11e286e4e9b203d1e1
CrazedHearts <jeromedemesillo@gmail.com>
2026-07-26 11:07:37 -1000

    PUSH PUSH PUSH

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const NavBar: React.FC = () => {
   const currentUser = session?.user?.email;
   const role = session?.user?.role;
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="white" expand="lg">
       <Container>
         <Navbar.Brand href="/">Logo goes here</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
